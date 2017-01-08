6fdde0208a99ad0eb33fe831f91148125a903e18
Jean-François Bercher <jf.bercher@gmail.com>
2017-01-09 00:33:46 +0100

    [nbTranslate] extract html structures before conversion and reinsert after

--- nbTranslate.js
+++ nbTranslate.js
@@ -153,7 +153,8 @@ function translateCurrentCell() {
     var cell = Jupyter.notebook.get_selected_cell();
     var cellText = cell.get_text();
     var maths_and_text = removeMaths(cellText)
-    var sourceText = maths_and_text[1]; 
+    var html_and_text = removeHtml(maths_and_text[1])
+    var sourceText = html_and_text[1]; 
     var mdReplacements = {'*': '<*>', '**': '<**>',
         '_': '<_>', '__': '<__>'}  
     // **, *, _, and __ in markdown are "protected" with <.> 
@@ -177,6 +178,7 @@ function translateCurrentCell() {
             }
             //console.log("Translated", translated_text) 
 
+            translated_text = restoreHtml([html_and_text[0], translated_text])
             translated_text = restoreMaths([maths_and_text[0], translated_text])
             translated_text = 
             translated_text.replace(/\\label{([\s\S]*?)}/g, function(m0,m1){return "\\label{"+m1+"_"+conf.targetLang+"}"})
@@ -244,7 +246,7 @@ function show_mdcells(displayLangs) {
                 if (typeof cell.metadata.lang == "undefined" || 
                          inArray(displayLangs, cell.metadata.lang) || 
                          inArray(displayLangs, '*')) {
-                    cell.render();
+                    if (!cell.rendered) cell.render();
                     cell.element.show();
                     lastmd_cell = cell;
                 }
